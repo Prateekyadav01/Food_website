e76ea2fe35bec772a4de477f33b145734680f8e5
PrateekTechmatys <prateeky562@gmail.com>
2024-01-13 11:14:55 +0530

    contact page is done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,31 @@
-const Contact =()=>{
-    return(
-        <div>
-            <h1>Contact</h1>
-        </div>
-    );
+import contact from "../images/Contact-Us.png";
+import { useState } from "react";
+
+const Contact = () => {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact-container">
+      <div className="contact-left">
+        <img src={contact} alt="contact" />
+      </div>
+      <div className="contact-right">
+        <h1>Contact us</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" required />
+          <input type="email" placeholder="Email" required />
+          <textarea placeholder="Type your Message here..." required></textarea>
+          <button type="submit">Submit</button>
+          {message && (
+            <span>Thanks for contacting MunchMate, We will reply ASAP.</span>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
