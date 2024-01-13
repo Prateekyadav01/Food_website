aff2cac34a1988d2b186778d51df463963d88812
PrateekTechmatys <prateeky562@gmail.com>
2024-01-13 11:03:25 +0530

    instamart is done

--- src/components/Instmart.js
+++ src/components/Instmart.js
@@ -1,54 +1,54 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Section =({title ,description,isVisible,setIsVisible})=>{
-    // const [ isVisible , setIsVisible]= useState(false);
-    return(
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+  // const [isVisible, setIsVisible] = useState(false);
+  return (
+    <>
+      <h2>{title}</h2>
+      {isVisible ? (
         <div>
-            <h1>{title}</h1>
-            {!isVisible ? 
-             <button onClick={()=>{setIsVisible(true)}}>Show</button>
-            :
-            <button onClick={()=>{setIsVisible(false)}}>hide</button>
-            }
-            {isVisible && <p>{description}</p>}
+          <button onClick={() => setIsVisible(null)}>Hide</button>
+          <p>{description}</p>
         </div>
-    )
-}
-const Instamart =()=>{
-    const [visible ,setVisible] = useState("about")
-    return (
-        <div>
-            <Section 
-              title="About us" 
-              description="This is About of Instamart"
-              isVisible={visible==="about"}
-              setIsVisible={()=>{
-                setVisible("about")
-              }}/>
-            <Section 
-              title="contact"
-               description="This is About of Instamart"
-               isVisible={visible==="contact"}
-              setIsVisible={()=>{
-                setVisible("contact")
-              }}/>
-            <Section 
-              title="phone" 
-              description="This is About of Instamart"
-              isVisible={visible==="phone"}
-              setIsVisible={()=>{
-                setVisible("phone")
-              }}/>
-            <Section 
-              title="button" 
-               description="This is About of Instamart"
-               isVisible={visible==="button"}
-              setIsVisible={()=>{
-                setVisible("button")
-              }}/>
-        </div>
-    )
-}
+      ) : (
+        <button onClick={() => setIsVisible(true)}>Show</button>
+      )}
+    </>
+  );
+};
 
+const InstaMart = () => {
+  const [visibleSection, setVisibleSection] = useState(null);
+  return (
+    <>
+      <Section
+        title={"About InstaMart"}
+        description={
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        }
+        isVisible={visibleSection === "about"}
+        setIsVisible={() => setVisibleSection("about")}
+      />
 
-export default Instamart;
+      <Section
+        title={"Team InstaMart"}
+        description={
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        }
+        isVisible={visibleSection === "team"}
+        setIsVisible={() => setVisibleSection("team")}
+      />
+
+      <Section
+        title={"Career at InstaMart"}
+        description={
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        }
+        isVisible={visibleSection === "career"}
+        setIsVisible={() => setVisibleSection("career")}
+      />
+    </>
+  );
+};
+
+export default InstaMart;
